ca467c4e6e9c768fe743ee6b1899e10971438965
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-19 11:34:00 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,5 +27,9 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  
+  if position_taken?(board, index) == false && index.between?(0, 8)
+    true
+  else
+    false
+  end
 end
