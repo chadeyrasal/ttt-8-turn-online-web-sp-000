e38304625e6ece582b5d5549c1bad3a90fafe151
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-19 11:11:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,4 @@
-board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+board = [" X ", " X ", " X ", " X ", " O ", " O ", " X ", " O ", " O "]
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
