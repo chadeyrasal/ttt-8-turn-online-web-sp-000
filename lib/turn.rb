abef2b1b5c9a0f477261e91cbcf55bef8e0b1aaa
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-19 11:09:08 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,8 @@
+board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+
+def display_board(board)
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
