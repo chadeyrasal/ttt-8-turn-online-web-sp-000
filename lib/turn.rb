1118e0eaf0089136c45f1b463660a865a896ab6a
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-19 11:18:07 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,4 @@
-
+board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -8,6 +8,5 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
 
-output = capture_puts{ display_board(board) }
+
